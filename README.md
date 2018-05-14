951c901553652305d8541b97487d9bf7b7115d54
KU Lok Sun <sundaykuloksun@gmail.com>
2018-05-14 10:09:48 +0800

    Add homebrew

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Vim
 
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+
+## Homebrew
+
+When setting up a new Mac, you may want to install some common [Homebrew](https://brew.sh/) formulae (after installing Homebrew, of course):
+
+```bash
+./brew.sh
+```
---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+
+brew install autojump
+brew install node
+brew install wget
+brew install yarn
+brew install zsh
+brew install zsh-completions
+
+brew cask install caffeine
+brew cask install clipy
+brew cask install docker
+brew cask install firefox
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install postman
+brew cask install sequel-pro
+brew cask install spectacle
+brew cask install sublime-text
+
+# Remove outdated versions from the cellar.
+brew cleanup
